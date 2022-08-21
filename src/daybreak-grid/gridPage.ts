a2429fde2e6c8da782fa995a5f879fa3b2a8de25
Alvin Leung <alvinleung2009@gmail.com>
2022-08-21 10:54:34 -0700

    log debug

--- src/daybreak-grid/gridPage.ts
+++ src/daybreak-grid/gridPage.ts
@@ -40,6 +40,7 @@ const getCellInfo = (
   onUpdate: Function
 ): CellInfo => {
   console.log(row);
+  console.log(template.content);
 
   const cellType = template.content[row][col];
   const currCellElmIndex = col + row * template.cols;
