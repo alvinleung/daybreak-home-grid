1b6e1f05ab0b177cf6861db70ff46274925301a8
Alvin Leung <alvinleung2009@gmail.com>
2022-08-21 13:43:05 -0700

    debug

--- src/daybreak-grid/grid.ts
+++ src/daybreak-grid/grid.ts
@@ -75,35 +75,6 @@ export const createInfiniteGrid = ({
 
   const scrollMotion = createSmoothMotion({ initial: 0, smoothFactor: 0.05 });
 
-  // const getPageAtIndex = (index: number) => {
-  //   const { positiveCount, negativeCount } = allPages.value.reduce(
-  //     (prev, curr) => {
-  //       if (curr.isInsertBefore) prev.negativeCount++;
-  //       else prev.positiveCount++;
-
-  //       return prev;
-  //     },
-  //     { positiveCount: 0, negativeCount: 0 }
-  //   );
-
-  //   console.log(positiveCount);
-  // };
-
-  const findFirstPage = (allPages: GridPage[]) => {
-    for (let i = 0; i < allPages.length; i++) {
-      const page = allPages[i];
-      if (page.isInsertBefore) return page;
-    }
-    return allPages[0];
-  };
-  const findLastPage = (allPages: GridPage[]) => {
-    for (let i = allPages.length - 1; i >= 0; i--) {
-      const page = allPages[i];
-      if (!page.isInsertBefore) return page;
-    }
-    return allPages[allPages.length - 1];
-  };
-
   createStateRenderer(() => {
     const handleScrollValueUpdate = (scroll: number) => {
       // use y position scroll when not using touch mode
@@ -144,6 +115,7 @@ export const createInfiniteGrid = ({
           });
 
           allPages.set([...allPages.value, newPage]);
+          attemptCreateNewPage();
           return;
         }
 
@@ -157,10 +129,9 @@ export const createInfiniteGrid = ({
           });
 
           allPages.set([...allPages.value, newPage]);
+          attemptCreateNewPage();
           return;
         }
-
-        attemptCreateNewPage();
       };
       requestAnimationFrame(attemptCreateNewPage);
     };
